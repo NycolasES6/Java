4cb629e7250835c6557a9eb07516d6436cfa2b49
NycolasES6 <nycolasonrails@gmail.com>
2023-06-23 15:32:18 -0300

    Start new project with thymeleaf
    Spring boot with thymeleaf

---
+++ estudos/src/springBoot/firstWebProject/src/test/java/com/example/firstWebProject/FirstWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
